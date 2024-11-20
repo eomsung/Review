418f8c885375a0e3bd2114df0d0f15dcf613ee00
sungmin <usm0259@kaist.ac.kr>
2024-11-20 18:41:12 +0900

    wip

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -2,17 +2,26 @@ import { useState } from "react";
 
 export const ReviewForm = () => {
   const [title, setTitle] = useState("");
+  const [rating, setRating] = useState(0);
+  const [content, setContent] = useState("");
 
   const handleTitleChange = (e) => {
     //e안에 input이 가지고 있는 속성들이 있음
     setTitle(e.target.value); //바뀐값
   };
+  const handleRatingChange = (e) => {
+    setRating(e.target.value) || 0;
+  };
+  const handleContentChange = (e) => {
+    //e안에 input이 가지고 있는 속성들이 있음
+    setContent(e.target.value); //바뀐값
+  };
 
   return (
     <form>
       <input value={title} onChange={handleTitleChange} />
-      <input type="number" />
-      <textarea />
+      <input type="number" onChange={handleRatingChange} />
+      <textarea onChange={handleContentChange} />
     </form>
   );
 };
